329a0598cfe0d6b8360ba57af21e6d687a193862
Frigyes Elemér <125876771+drt09@users.noreply.github.com>
2023-06-06 19:18:16 +0200

    próbálkozás törölve, kosárhoz adás tesztelés

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,10 @@
 var bnumber = document.getElementById("item-counter")
 var fn = document.getElementById("fn")
 
-var cv = document.getElementById("checkbox-v").checked
-var sznev = document.getElementById("nevszamlazas").innerHTML
-var szirsz = document.getElementById("irszszamlazas")
-var sztko = document.getElementById("Tlpszamlazas")
-var szalnev = document.getElementById("nevszallitas").innerHTML
-var szalirsz = document.getElementById("irszszallitas")
-var szaltlp = document.getElementById("Tlpszallitas")
-
 function BasketAdd()
 {
     for (let i = 0; i < fn.value; i++)
     {
         bnumber.innerHTML += i+1
     }
-
 }
