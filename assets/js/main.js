8bcf6ee3ef5f558fc2606683a267e94e569d0054
Frigyes Elemér <125876771+drt09@users.noreply.github.com>
2023-06-06 21:59:30 +0200

    kosár végösszeg + törlés próbálgatása

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 var bnumber = document.getElementById("item-counter")
+var vg = document.getElementById("vegosszeg")
+var fntd1 = document.getElementById("fntd1")
 var fn1 = document.getElementById("fn1")
 var fn2 = document.getElementById("fn2")
 var fn3 = document.getElementById("fn3")
@@ -9,10 +11,12 @@ var fn6 = document.getElementById("fn6")
 function BasketAdd()
 {
     var osszeg = 0
+    var vegosszeg = 0
     for (let i = 0; i < fn1.value; i++)
     {
         osszeg++
         bnumber.innerHTML = osszeg
+        fntd1.innerHTML += '<button onclick="Delete()">asd</button>'
     }
     for (let i = 0; i < fn2.value; i++)
     {
@@ -39,4 +43,6 @@ function BasketAdd()
         osszeg++
         bnumber.innerHTML = osszeg
     }
+    vegosszeg = osszeg*1500
+    vg.innerHTML = vegosszeg
 }
