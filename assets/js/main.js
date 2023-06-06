ccc957a06af183b305278ee420a939ab89c32a00
Frigyes Elemér <125876771+drt09@users.noreply.github.com>
2023-06-06 21:48:56 +0200

    kosárhoz számolás

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,42 @@
 var bnumber = document.getElementById("item-counter")
-var fn = document.getElementById("fn")
+var fn1 = document.getElementById("fn1")
+var fn2 = document.getElementById("fn2")
+var fn3 = document.getElementById("fn3")
+var fn4 = document.getElementById("fn4")
+var fn5 = document.getElementById("fn5")
+var fn6 = document.getElementById("fn6")
 
 function BasketAdd()
 {
-    for (let i = 0; i < fn.value; i++)
+    var osszeg = 0
+    for (let i = 0; i < fn1.value; i++)
     {
-        bnumber.innerHTML += i+1
+        osszeg++
+        bnumber.innerHTML = osszeg
+    }
+    for (let i = 0; i < fn2.value; i++)
+    {
+        osszeg++
+        bnumber.innerHTML = osszeg
+    }
+    for (let i = 0; i < fn3.value; i++)
+    {
+        osszeg++
+        bnumber.innerHTML = osszeg
+    }
+    for (let i = 0; i < fn4.value; i++)
+    {
+        osszeg++
+        bnumber.innerHTML = osszeg
+    }
+    for (let i = 0; i < fn5.value; i++)
+    {
+        osszeg++
+        bnumber.innerHTML = osszeg
+    }
+    for (let i = 0; i < fn6.value; i++)
+    {
+        osszeg++
+        bnumber.innerHTML = osszeg
     }
 }
