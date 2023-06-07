895b5abd2b52cd899ec72e63e6d24e77fac1169c
Bingobalazs <thetoazti@gmail.com>
2023-06-07 10:09:49 +0200

    termék kiírás (hibás)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 var bnumber = document.getElementById("item-counter")
 var vg = document.getElementById("vegosszeg")
-var fntd1 = document.getElementById("fntd1")
+var fntd = document.getElementById("fntd")
 var fn1 = document.getElementById("fn1")
 var fn2 = document.getElementById("fn2")
 var fn3 = document.getElementById("fn3")
 var fn4 = document.getElementById("fn4")
 var fn5 = document.getElementById("fn5")
 var fn6 = document.getElementById("fn6")
+var osszeg = 0
+var vegosszeg = 0
 
 function BasketAdd()
 {
-    var osszeg = 0
-    var vegosszeg = 0
     for (let i = 0; i < fn1.value; i++)
     {
         osszeg++
         bnumber.innerHTML = osszeg
-        fntd1.innerHTML += '<button onclick="Delete()">asd</button>'
+        fntd.innerHTML += '<p id="GG" onclick="Delete()">Gourmet Gold</p>'
     }
     for (let i = 0; i < fn2.value; i++)
     {
@@ -45,4 +45,8 @@ function BasketAdd()
     }
     vegosszeg = osszeg*1500
     vg.innerHTML = vegosszeg
+}
+
+function Delete(){
+    
 }
